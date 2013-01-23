a4640089984723cff5662b9dd7971f05aa1560a0
Jon Lundy <jon@xuu.cc>
2013-01-23 16:02:18 -0700

    rename of submodule

--- public/app.php
+++ public/app.php
@@ -1,7 +1,8 @@
 <?php namespace XeptoAuthCAS;
 
 use Xepto;
-use XeptoCheck;
+use XeptoAuthToken;
+use XeptoAuthCAS;
 
 chdir($_SERVER['DOCUMENT_ROOT']);
 set_include_path(get_include_path().':lib/');
@@ -23,9 +24,9 @@ $config->merge(require "XeptoAuthCAS/config/auth.$env.php");
 
 $response = new Xepto\Request\Response ($config->response, $request);
 
-$persist    = new XeptoCheck\Token\Persist   ($config->persist);
-$encryption = new XeptoCheck\Token\Encryption(MCRYPT_BlOWFISH, MCRYPT_MODE_CBC);
-$token      = new XeptoCheck\Token\Token     ($config->token, $request, $response, $persist, $encryption);
+$persist    = new XeptoAuthToken\Token\Persist   ($config->persist);
+$encryption = new XeptoAuthToken\Token\Encryption(MCRYPT_BlOWFISH, MCRYPT_MODE_CBC);
+$token      = new XeptoAuthToken\Token\Token     ($config->token, $request, $response, $persist, $encryption);
 
 $router   = new Xepto\Request\Router   ($config->router,   $request, $response, $token);
 
