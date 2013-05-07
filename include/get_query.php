46ec41b0123b818d8d0d9088f07a8b7b90721f5e
Jon Lundy <Jon.Lundy@hsc.utah.edu>
2013-05-07 13:12:15 -0600

    Remove POST globals

--- include/get_query.php
+++ include/get_query.php
@@ -2,10 +2,6 @@
 
 //!--- [ get_query ] --------------
 
-
-$_RAWPOST = []; 
-$_POST = [];
-
 {  // Process the Get header
     $ex = explode('?',$_SERVER['REQUEST_URI']);
     if (count($ex) > 1) parse_str($ex[1],$_GET);
