1d920c08e75117f8285f0edcfc832e902d0bbe47
Jon Lundy <Jon.Lundy@hsc.utah.edu>
2013-05-07 13:12:42 -0600

    Remove redundant str_replace.

--- include/get_headers.php
+++ include/get_headers.php
@@ -8,7 +8,7 @@ if (!function_exists('getallheaders')) {
     {
         foreach ($_SERVER as $key=>$value) {
             if (substr($key,0,5)=="HTTP_") {
-                $key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5)))));
+                $key=ucwords(strtolower(str_replace("_","-",substr($key,5))));
                 $out[$key]=$value;
             } else {
                 $out[$key]=$value;
